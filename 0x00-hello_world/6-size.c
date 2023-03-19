0fc1523b28ede97d5fb0f93fbd8afa8e33ed3c23
kemunto <beekeiy50@gmail.com>
2023-03-19 02:06:40 -0700

    c 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
- * return: 0 if correc
- * :t
+/**
+ * main - main function
+ *
+ * return: o on success
  */
 
 int main(void)
@@ -9,7 +11,7 @@ int main(void)
 printf("Size of a char: %lu byte(s)", sizeof(char));
 printf("size of int: %lu byte(s)", sizeof(int));
 printf("size of a long int: %lu byte(s)", sizeof(long int);
-printf("size of a long long int: %lu byte(s):, sizeof(long long int));
+printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 printf("size of a float : % lu byte(s)", sizeof(float));
 	return (0);
 }
