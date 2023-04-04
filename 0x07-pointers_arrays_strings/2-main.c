7d66582d57ac35fd07be8259667f7fcc9603b929
kemunto <beekeiy50@gmail.com>
2023-04-04 05:06:59 -0700

    task2

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -1,5 +1,5 @@
-include " main.h "
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -11,7 +11,7 @@ int main(void)
 	char *s = "hello";
 	char *f;
 
-	f = _ strchr(s, 'l');
+	f = strchr(s, 'l');
 
 	if (f != NULL)
 	{
