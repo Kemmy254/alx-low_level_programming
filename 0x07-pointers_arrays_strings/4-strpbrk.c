f37c01b2167c8192bfc3a82e998889c81118ff4b
kemunto <beekeiy50@gmail.com>
2023-04-04 04:11:04 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the set of bytes to search for
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * arduino
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
