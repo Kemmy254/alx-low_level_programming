312a0494301f2dd3fbbe6606f3f62e75ed8f5f17
kemunto <beekeiy50@gmail.com>
2023-04-10 14:03:27 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,6 @@
 
 int _atoi(char *s)
 {
-
 	int i, d, n, len, f, digit;
 
 	i = 0;
@@ -22,34 +21,60 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
-	if (s[i] == '-')
-	{
-	++d;
+		if (s[i] == '-')
+			++d;
 	}
-
 	if (s[i] >= '0' && s[i] <= '9')
 	{
-	digit = s[i] - '0';
+		digit = s[i] - '0';
+		if (d % 2)
+			digit = -digit;
 
-	if (d % 2)
-		digit = -digit;
-
-	n = n * 10 + digit;
+		n = n * 10 + digit;
 		f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9'){
-		break;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 
 		f = 0;
 	}
 	i++;
 
-	}
 
 	if (f == 0)
 
 		return (0);
 
 	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+
 }
